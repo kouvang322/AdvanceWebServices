f1551a2e2c53841445c6a22a20a6c3dd38c656f0
kouvang322 <kouvang322@hotmail.com>
2023-01-27 00:12:49 -0600

    assignment 2

--- medals/src/App.js
+++ medals/src/App.js
@@ -1,18 +1,35 @@
 // import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 import Country from './components/Country';
 
-function App() {
-  return (
-    <div className="App" style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-    }}>
-        <Country />
-    </div>
-  );
+class App extends Component {
+  state = {
+    countries: [
+      { id: 1, name: 'United States', goldMedalCount: 2 },
+      { id: 2, name: 'China', goldMedalCount: 3 },
+      { id: 3, name: 'Germany', goldMedalCount: 0 },
+    ]
+  }
+
+  render(){
+    console.log(this.props)
+    return (
+      <div className="App" style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}>
+        { this.state.countries.map(country => 
+        <Country
+          key={ country.id}
+          name= {country.name}
+          goldMedalCount={ country.goldMedalCount}
+        />) }
+      </div>
+    );
+  }
 }
 
 export default App;
--- medals/src/components/Country.jsx
+++ medals/src/components/Country.jsx
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 import { Avatar, Card, Divider, Fab } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import { yellow } from "@mui/material/colors";
 
 class Country extends Component {
     state = {
-        name: 'United States',
-        gold: 0,
+        name: this.props.name,
+        goldMedalCount: this.props.goldMedalCount,
     }
 
-    handleClick = () => {
+    handleAdd = () => {
         // checking current gold count before adding 1
         // console.log(this.state.gold);
 
         // adding one to the current gold count
-        this.setState({ gold: this.state.gold + 1 });
+        this.setState({ goldMedalCount: this.state.goldMedalCount + 1 });
+    }
+
+    handleMinus = () =>{
+        // checking if function is connected to button
+        // console.log("minus");
+
+        // minus 1 from gold medal count
+        this.setState({ goldMedalCount: this.state.goldMedalCount - 1})
     }
 
     render() {
         const myStyle = {
             width: "300px",
-            height: "125px",
+            height: "150px",
         }
 
         return (
@@ -32,11 +41,14 @@ class Country extends Component {
                     </div>
                     <Divider />
                     <div className="Gold">
-                        Gold Medals:<Avatar sx={{ bgcolor: yellow[700], width: 50, height: 50 }} ><WorkspacePremiumIcon />{this.state.gold}</Avatar> <Fab color="success" size="medium" onClick={this.handleClick}><AddIcon /></Fab>
+                        Gold Medals
+                        <Avatar sx={{ bgcolor: yellow[700], width: 50, height: 50 }} ><WorkspacePremiumIcon />{this.state.goldMedalCount}</Avatar> 
+                        <Fab color="success" size="medium" onClick={this.handleAdd}><AddIcon /></Fab>
+                        <Fab color="error" size="medium" onClick={this.handleMinus} disabled={this.state.goldMedalCount === 0 ? true: false}><RemoveIcon /></Fab>
+                        
                     </div>
                 </div>
             </Card>
-
         );
     }
 }
